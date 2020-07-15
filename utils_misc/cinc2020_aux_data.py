30b192e5db4a900d272e43be68c0f0e3465f138c
WEN Hao <wenh06@gmail.com>
2020-07-15 08:08:35 +0800

    update cinc2020 scoring aux data

--- utils_misc/cinc2020_aux_data.py
+++ utils_misc/cinc2020_aux_data.py
@@ -1,135 +1,56 @@
-# -*- coding: utf-8 -*-
 """
-auxiliary data for the PhysioNet Challenge 2020
+from 3 files of the official evaluation repo:
 
-References:
------------
-[1] https://github.com/physionetchallenges/physionetchallenges.github.io/blob/master/2020/Dx_map.csv (removed)
-[2] https://github.com/physionetchallenges/evaluation-2020/blob/master/dx_mapping_scored.csv
-[3] https://github.com/physionetchallenges/evaluation-2020/blob/master/dx_mapping_unscored.csv
-[4] https://github.com/physionetchallenges/evaluation-2020/blob/master/weights.csv
+    dx_mapping_scored.csv, dx_mapping_unscored.csv, weights.csv
 """
 from io import StringIO
+from typing import Union, Sequence
 
 import pandas as pd
 
 
 __all__ = [
-    "Dx_map",
-    "dx_mapping_scored_cinc2020", "dx_mapping_unscored_cinc2020",
-    "scoring_matrix_cinc2020",
+    "df_weights",
+    "df_weights_abbr",
+    "dx_mapping_scored",
+    "dx_mapping_unscored",
+    "dx_mapping_all",
+    "load_weights",
 ]
 
 
-# NOTE that 'SNR' is the 'Normal' rhythm
-Dx_map = pd.read_csv(StringIO("""dx,SNOMED code,Abbreviation
-1st degree av block,270492004,IAVB
-2nd degree av block,195042002,IIAVB
-accelerated idioventricular rhythm,61277005,AIVR
-accelerated junctional rhythm,426664006,AJR
-acute myocardial ischemia,413444003,AMIs
-anterior ischemia,426434006,AnMIs
-anterior myocardial infarction,54329005,AnMI
-atrial fibrillation,164889003,AF
-atrial fibrillation and flutter,195080001,AFAFL
-atrial flutter,164890007,AFL
-atrial hypertrophy,195126007,AH
-atrial pacing pattern,251268003,AP
-atrial tachycardia,713422000,ATach
-av block,233917008,AVB
-blocked premature atrial contraction,251170000,BPAC
-brady tachy syndrome,74615001,BTS
-bradycardia,426627000,Brady
-brugada syndrome,418818005,Brug
-bundle branch block,6374002,BBB
-chronic atrial fibrillation,426749004,CAF
-chronic myocardial ischemia,413844008,CMIs
-chronic rheumatic pericarditis,78069008,CRPC
-complete heart block,27885002,CHB
-complete right bundle branch block,713427006,CRBBB
-decreased qt interval,77867006,SQT
-diffuse intraventricular block,82226007,DIVB
-early repolarization,428417006,ERe
-ecg artefacts,251143007,ART
-ectopic rhythm,29320008,ER
-electrical alternans,423863005,EA
-high t voltage,251259000,HTV
-incomplete left bundle branch block,251120003,ILBBB
-incomplete right bundle branch block,713426002,IRBBB
-indeterminate cardiac axis,251200008,ICA
-inferior ischaemia,425419005,IIs
-inferior st segment depression,704997005,ISTD
-isorhythmic dissociation,50799005,IAVD
-junctional escape,426995002,JE
-junctional premature complex,251164006,JPC
-junctional tachycardia,426648003,JTach
-lateral ischaemia,425623009,LIs
-left anterior fascicular block,445118002,LAnFB
-left atrial abnormality,253352002,LAA
-left atrial enlargement,67741000119109,LAE
-left atrial hypertrophy,446813000,LAH
-left axis deviation,39732003,LAD
-left bundle branch block,164909002,LBBB
-left posterior fascicular block,445211001,LPFB
-left ventricular hypertrophy,164873001,LVH
-left ventricular strain,370365005,LVS
-low qrs voltages,251146004,LQRSV
-low qrs voltages in the limb leads,251147008,LQRSVLL
-low qrs voltages in the precordial leads,251148003,LQRSP
-mobitz type 2 second degree atrioventricular block,28189009,MoII
-mobitz type i wenckebach atrioventricular block,54016002,MoI
-multifocal atrial tachycardia,713423005,MATach
-myocardial infarction,164865005,MI
-myocardial ischemia,164861001,MIs
-non-specific interatrial conduction block,65778007,NSIACB
-nonspecific intraventricular conduction disorder,698252002,NSIVCB
-nonspecific st t abnormality,428750005,NSSTTA
-old myocardial infarction,164867002,OldMI
-pacing rhythm,10370003,PR
-paroxysmal supraventricular tachycardia,67198005,PSVT
-partial atrioventricular block 2:1,164903001,PAVB21
-premature atrial contraction,284470004,PAC
-premature ventricular complexes,164884008,PVC
-premature ventricular contractions,427172004,PVC
-prolonged qt interval,111975006,LQT
-qwave abnormal,164917005,QAb
-r wave abnormal,164921003,RAb
-right atrial abnormality,253339007,RAAb
-right atrial hypertrophy,446358003,RAH
-right axis deviation,47665007,RAD
-right bundle branch block,59118001,RBBB
-right ventricular hypertrophy,89792004,RVH
-s t changes,55930002,STC
-shortened pr interval,49578007,SPRI
-sinus arrhythmia,427393009,SA
-sinus bradycardia,426177001,SB
-sinus rhythm,426783006,SNR
-sinus tachycardia,427084000,STach
-st depression,429622005,STD
-st elevation,164931005,STE
-st interval abnormal,164930006,STIAb
-supraventricular premature beats,63593006,SVPB
-supraventricular tachycardia,426761007,SVT
-suspect arm ecg leads reversed,251139008,ALR
-t wave abnormal,164934002,TAb
-t wave inversion,59931005,TInv
-tall u wave,251242005,UTall
-u wave abnormal,164937009,UAb
-ventricular bigeminy,11157007,VBig
-ventricular ectopic beats,17338001,VEB
-ventricular escape beat,75532003,VEsB
-ventricular escape rhythm,81898007,VEsR
-ventricular fibrillation,164896001,VF
-ventricular flutter,111288001,VFL
-ventricular hypertrophy,266249003,VH
-ventricular pacing pattern,251266004,VPP
-ventricular pre excitation,195060002,VPEx
-ventricular tachycardia,164895002,VTach
-ventricular trigeminy,251180001,VTrig
-wandering atrial pacemaker,195101003,WAP
-wolff parkinson white pattern,74390002,WPW"""))
+df_weights = pd.read_csv(StringIO(""",270492004,164889003,164890007,426627000,713427006,713426002,445118002,39732003,164909002,251146004,698252002,10370003,284470004,427172004,164947007,111975006,164917005,47665007,59118001,427393009,426177001,426783006,427084000,63593006,164934002,59931005,17338001
+270492004,1.0,0.3,0.3,0.5,0.4,0.5,0.45,0.45,0.325,0.375,0.45,0.425,0.4625,0.425,0.5,0.35,0.2,0.45,0.4,0.5,0.5,0.45,0.425,0.4625,0.3,0.3,0.425
+164889003,0.3,1.0,0.5,0.3,0.4,0.3,0.35,0.35,0.475,0.425,0.35,0.375,0.3375,0.375,0.3,0.45,0.4,0.35,0.4,0.3,0.3,0.25,0.375,0.3375,0.5,0.5,0.375
+164890007,0.3,0.5,1.0,0.3,0.4,0.3,0.35,0.35,0.475,0.425,0.35,0.375,0.3375,0.375,0.3,0.45,0.4,0.35,0.4,0.3,0.3,0.25,0.375,0.3375,0.5,0.5,0.375
+426627000,0.5,0.3,0.3,1.0,0.4,0.5,0.45,0.45,0.325,0.375,0.45,0.425,0.4625,0.425,0.5,0.35,0.2,0.45,0.4,0.5,0.5,0.45,0.425,0.4625,0.3,0.3,0.425
+713427006,0.4,0.4,0.4,0.4,1.0,0.4,0.45,0.45,0.425,0.475,0.45,0.475,0.4375,0.475,0.4,0.45,0.3,0.45,1.0,0.4,0.4,0.35,0.475,0.4375,0.4,0.4,0.475
+713426002,0.5,0.3,0.3,0.5,0.4,1.0,0.45,0.45,0.325,0.375,0.45,0.425,0.4625,0.425,0.5,0.35,0.2,0.45,0.4,0.5,0.5,0.45,0.425,0.4625,0.3,0.3,0.425
+445118002,0.45,0.35,0.35,0.45,0.45,0.45,1.0,0.5,0.375,0.425,0.5,0.475,0.4875,0.475,0.45,0.4,0.25,0.5,0.45,0.45,0.45,0.4,0.475,0.4875,0.35,0.35,0.475
+39732003,0.45,0.35,0.35,0.45,0.45,0.45,0.5,1.0,0.375,0.425,0.5,0.475,0.4875,0.475,0.45,0.4,0.25,0.5,0.45,0.45,0.45,0.4,0.475,0.4875,0.35,0.35,0.475
+164909002,0.325,0.475,0.475,0.325,0.425,0.325,0.375,0.375,1.0,0.45,0.375,0.4,0.3625,0.4,0.325,0.475,0.375,0.375,0.425,0.325,0.325,0.275,0.4,0.3625,0.475,0.475,0.4
+251146004,0.375,0.425,0.425,0.375,0.475,0.375,0.425,0.425,0.45,1.0,0.425,0.45,0.4125,0.45,0.375,0.475,0.325,0.425,0.475,0.375,0.375,0.325,0.45,0.4125,0.425,0.425,0.45
+698252002,0.45,0.35,0.35,0.45,0.45,0.45,0.5,0.5,0.375,0.425,1.0,0.475,0.4875,0.475,0.45,0.4,0.25,0.5,0.45,0.45,0.45,0.4,0.475,0.4875,0.35,0.35,0.475
+10370003,0.425,0.375,0.375,0.425,0.475,0.425,0.475,0.475,0.4,0.45,0.475,1.0,0.4625,0.5,0.425,0.425,0.275,0.475,0.475,0.425,0.425,0.375,0.5,0.4625,0.375,0.375,0.5
+284470004,0.4625,0.3375,0.3375,0.4625,0.4375,0.4625,0.4875,0.4875,0.3625,0.4125,0.4875,0.4625,1.0,0.4625,0.4625,0.3875,0.2375,0.4875,0.4375,0.4625,0.4625,0.4125,0.4625,1.0,0.3375,0.3375,0.4625
+427172004,0.425,0.375,0.375,0.425,0.475,0.425,0.475,0.475,0.4,0.45,0.475,0.5,0.4625,1.0,0.425,0.425,0.275,0.475,0.475,0.425,0.425,0.375,0.5,0.4625,0.375,0.375,1.0
+164947007,0.5,0.3,0.3,0.5,0.4,0.5,0.45,0.45,0.325,0.375,0.45,0.425,0.4625,0.425,1.0,0.35,0.2,0.45,0.4,0.5,0.5,0.45,0.425,0.4625,0.3,0.3,0.425
+111975006,0.35,0.45,0.45,0.35,0.45,0.35,0.4,0.4,0.475,0.475,0.4,0.425,0.3875,0.425,0.35,1.0,0.35,0.4,0.45,0.35,0.35,0.3,0.425,0.3875,0.45,0.45,0.425
+164917005,0.2,0.4,0.4,0.2,0.3,0.2,0.25,0.25,0.375,0.325,0.25,0.275,0.2375,0.275,0.2,0.35,1.0,0.25,0.3,0.2,0.2,0.15,0.275,0.2375,0.4,0.4,0.275
+47665007,0.45,0.35,0.35,0.45,0.45,0.45,0.5,0.5,0.375,0.425,0.5,0.475,0.4875,0.475,0.45,0.4,0.25,1.0,0.45,0.45,0.45,0.4,0.475,0.4875,0.35,0.35,0.475
+59118001,0.4,0.4,0.4,0.4,1.0,0.4,0.45,0.45,0.425,0.475,0.45,0.475,0.4375,0.475,0.4,0.45,0.3,0.45,1.0,0.4,0.4,0.35,0.475,0.4375,0.4,0.4,0.475
+427393009,0.5,0.3,0.3,0.5,0.4,0.5,0.45,0.45,0.325,0.375,0.45,0.425,0.4625,0.425,0.5,0.35,0.2,0.45,0.4,1.0,0.5,0.45,0.425,0.4625,0.3,0.3,0.425
+426177001,0.5,0.3,0.3,0.5,0.4,0.5,0.45,0.45,0.325,0.375,0.45,0.425,0.4625,0.425,0.5,0.35,0.2,0.45,0.4,0.5,1.0,0.45,0.425,0.4625,0.3,0.3,0.425
+426783006,0.45,0.25,0.25,0.45,0.35,0.45,0.4,0.4,0.275,0.325,0.4,0.375,0.4125,0.375,0.45,0.3,0.15,0.4,0.35,0.45,0.45,1.0,0.375,0.4125,0.25,0.25,0.375
+427084000,0.425,0.375,0.375,0.425,0.475,0.425,0.475,0.475,0.4,0.45,0.475,0.5,0.4625,0.5,0.425,0.425,0.275,0.475,0.475,0.425,0.425,0.375,1.0,0.4625,0.375,0.375,0.5
+63593006,0.4625,0.3375,0.3375,0.4625,0.4375,0.4625,0.4875,0.4875,0.3625,0.4125,0.4875,0.4625,1.0,0.4625,0.4625,0.3875,0.2375,0.4875,0.4375,0.4625,0.4625,0.4125,0.4625,1.0,0.3375,0.3375,0.4625
+164934002,0.3,0.5,0.5,0.3,0.4,0.3,0.35,0.35,0.475,0.425,0.35,0.375,0.3375,0.375,0.3,0.45,0.4,0.35,0.4,0.3,0.3,0.25,0.375,0.3375,1.0,0.5,0.375
+59931005,0.3,0.5,0.5,0.3,0.4,0.3,0.35,0.35,0.475,0.425,0.35,0.375,0.3375,0.375,0.3,0.45,0.4,0.35,0.4,0.3,0.3,0.25,0.375,0.3375,0.5,1.0,0.375
+17338001,0.425,0.375,0.375,0.425,0.475,0.425,0.475,0.475,0.4,0.45,0.475,0.5,0.4625,1.0,0.425,0.425,0.275,0.475,0.475,0.425,0.425,0.375,0.5,0.4625,0.375,0.375,1.0"""), index_col=0)
+df_weights.index = df_weights.index.map(str)
 
-dx_mapping_scored_cinc2020 = pd.read_csv(StringIO("""Dx,SNOMED CT Code,Abbreviation,CPSC,CPSC-Extra,StPetersburg,PTB,PTB-XL,Georgia,Total,Notes
+
+dx_mapping_scored = pd.read_csv(StringIO("""Dx,SNOMED CT Code,Abbreviation,CPSC,CPSC-Extra,StPetersburg,PTB,PTB-XL,Georgia,Total,Notes
 1st degree av block,270492004,IAVB,722,106,0,0,797,769,2394,
 atrial fibrillation,164889003,AF,1221,153,2,15,1514,570,3475,
 atrial flutter,164890007,AFL,0,54,0,1,73,186,314,
@@ -158,7 +79,8 @@ t wave abnormal,164934002,TAb,0,22,0,0,2345,2306,4673,
 t wave inversion,59931005,TInv,0,5,1,0,294,812,1112,
 ventricular premature beats,17338001,VPB,0,8,0,0,0,357,365,We score 427172004 and 17338001 as the same diagnosis."""))
 
-dx_mapping_unscored_cinc2020 = pd.read_csv(StringIO("""Dx,SNOMED CT Code,Abbreviation,CPSC,CPSC-Extra,StPetersburg,PTB,PTB-XL,Georgia,Total
+
+dx_mapping_unscored = pd.read_csv(StringIO("""Dx,SNOMED CT Code,Abbreviation,CPSC,CPSC-Extra,StPetersburg,PTB,PTB-XL,Georgia,Total
 2nd degree av block,195042002,IIAVB,0,21,0,0,14,23,58
 abnormal QRS,164951009,abQRS,0,0,0,0,3389,0,3389
 accelerated junctional rhythm,426664006,AJR,0,0,0,0,0,19,19
@@ -244,34 +166,87 @@ ventricular trigeminy,251180001,VTrig,0,4,4,0,20,1,29
 wandering atrial pacemaker,195101003,WAP,0,0,0,0,0,7,7
 wolff parkinson white pattern,74390002,WPW,0,0,4,2,80,2,88"""))
 
-weights_cinc2020 = pd.read_csv(StringIO(""",270492004,164889003,164890007,426627000,713427006,713426002,445118002,39732003,164909002,251146004,698252002,10370003,284470004,427172004,164947007,111975006,164917005,47665007,59118001,427393009,426177001,426783006,427084000,63593006,164934002,59931005,17338001
-270492004,1.0,0.3,0.3,0.5,0.4,0.5,0.45,0.45,0.325,0.375,0.45,0.425,0.4625,0.425,0.5,0.35,0.2,0.45,0.4,0.5,0.5,0.45,0.425,0.4625,0.3,0.3,0.425
-164889003,0.3,1.0,0.5,0.3,0.4,0.3,0.35,0.35,0.475,0.425,0.35,0.375,0.3375,0.375,0.3,0.45,0.4,0.35,0.4,0.3,0.3,0.25,0.375,0.3375,0.5,0.5,0.375
-164890007,0.3,0.5,1.0,0.3,0.4,0.3,0.35,0.35,0.475,0.425,0.35,0.375,0.3375,0.375,0.3,0.45,0.4,0.35,0.4,0.3,0.3,0.25,0.375,0.3375,0.5,0.5,0.375
-426627000,0.5,0.3,0.3,1.0,0.4,0.5,0.45,0.45,0.325,0.375,0.45,0.425,0.4625,0.425,0.5,0.35,0.2,0.45,0.4,0.5,0.5,0.45,0.425,0.4625,0.3,0.3,0.425
-713427006,0.4,0.4,0.4,0.4,1.0,0.4,0.45,0.45,0.425,0.475,0.45,0.475,0.4375,0.475,0.4,0.45,0.3,0.45,1.0,0.4,0.4,0.35,0.475,0.4375,0.4,0.4,0.475
-713426002,0.5,0.3,0.3,0.5,0.4,1.0,0.45,0.45,0.325,0.375,0.45,0.425,0.4625,0.425,0.5,0.35,0.2,0.45,0.4,0.5,0.5,0.45,0.425,0.4625,0.3,0.3,0.425
-445118002,0.45,0.35,0.35,0.45,0.45,0.45,1.0,0.5,0.375,0.425,0.5,0.475,0.4875,0.475,0.45,0.4,0.25,0.5,0.45,0.45,0.45,0.4,0.475,0.4875,0.35,0.35,0.475
-39732003,0.45,0.35,0.35,0.45,0.45,0.45,0.5,1.0,0.375,0.425,0.5,0.475,0.4875,0.475,0.45,0.4,0.25,0.5,0.45,0.45,0.45,0.4,0.475,0.4875,0.35,0.35,0.475
-164909002,0.325,0.475,0.475,0.325,0.425,0.325,0.375,0.375,1.0,0.45,0.375,0.4,0.3625,0.4,0.325,0.475,0.375,0.375,0.425,0.325,0.325,0.275,0.4,0.3625,0.475,0.475,0.4
-251146004,0.375,0.425,0.425,0.375,0.475,0.375,0.425,0.425,0.45,1.0,0.425,0.45,0.4125,0.45,0.375,0.475,0.325,0.425,0.475,0.375,0.375,0.325,0.45,0.4125,0.425,0.425,0.45
-698252002,0.45,0.35,0.35,0.45,0.45,0.45,0.5,0.5,0.375,0.425,1.0,0.475,0.4875,0.475,0.45,0.4,0.25,0.5,0.45,0.45,0.45,0.4,0.475,0.4875,0.35,0.35,0.475
-10370003,0.425,0.375,0.375,0.425,0.475,0.425,0.475,0.475,0.4,0.45,0.475,1.0,0.4625,0.5,0.425,0.425,0.275,0.475,0.475,0.425,0.425,0.375,0.5,0.4625,0.375,0.375,0.5
-284470004,0.4625,0.3375,0.3375,0.4625,0.4375,0.4625,0.4875,0.4875,0.3625,0.4125,0.4875,0.4625,1.0,0.4625,0.4625,0.3875,0.2375,0.4875,0.4375,0.4625,0.4625,0.4125,0.4625,1.0,0.3375,0.3375,0.4625
-427172004,0.425,0.375,0.375,0.425,0.475,0.425,0.475,0.475,0.4,0.45,0.475,0.5,0.4625,1.0,0.425,0.425,0.275,0.475,0.475,0.425,0.425,0.375,0.5,0.4625,0.375,0.375,1.0
-164947007,0.5,0.3,0.3,0.5,0.4,0.5,0.45,0.45,0.325,0.375,0.45,0.425,0.4625,0.425,1.0,0.35,0.2,0.45,0.4,0.5,0.5,0.45,0.425,0.4625,0.3,0.3,0.425
-111975006,0.35,0.45,0.45,0.35,0.45,0.35,0.4,0.4,0.475,0.475,0.4,0.425,0.3875,0.425,0.35,1.0,0.35,0.4,0.45,0.35,0.35,0.3,0.425,0.3875,0.45,0.45,0.425
-164917005,0.2,0.4,0.4,0.2,0.3,0.2,0.25,0.25,0.375,0.325,0.25,0.275,0.2375,0.275,0.2,0.35,1.0,0.25,0.3,0.2,0.2,0.15,0.275,0.2375,0.4,0.4,0.275
-47665007,0.45,0.35,0.35,0.45,0.45,0.45,0.5,0.5,0.375,0.425,0.5,0.475,0.4875,0.475,0.45,0.4,0.25,1.0,0.45,0.45,0.45,0.4,0.475,0.4875,0.35,0.35,0.475
-59118001,0.4,0.4,0.4,0.4,1.0,0.4,0.45,0.45,0.425,0.475,0.45,0.475,0.4375,0.475,0.4,0.45,0.3,0.45,1.0,0.4,0.4,0.35,0.475,0.4375,0.4,0.4,0.475
-427393009,0.5,0.3,0.3,0.5,0.4,0.5,0.45,0.45,0.325,0.375,0.45,0.425,0.4625,0.425,0.5,0.35,0.2,0.45,0.4,1.0,0.5,0.45,0.425,0.4625,0.3,0.3,0.425
-426177001,0.5,0.3,0.3,0.5,0.4,0.5,0.45,0.45,0.325,0.375,0.45,0.425,0.4625,0.425,0.5,0.35,0.2,0.45,0.4,0.5,1.0,0.45,0.425,0.4625,0.3,0.3,0.425
-426783006,0.45,0.25,0.25,0.45,0.35,0.45,0.4,0.4,0.275,0.325,0.4,0.375,0.4125,0.375,0.45,0.3,0.15,0.4,0.35,0.45,0.45,1.0,0.375,0.4125,0.25,0.25,0.375
-427084000,0.425,0.375,0.375,0.425,0.475,0.425,0.475,0.475,0.4,0.45,0.475,0.5,0.4625,0.5,0.425,0.425,0.275,0.475,0.475,0.425,0.425,0.375,1.0,0.4625,0.375,0.375,0.5
-63593006,0.4625,0.3375,0.3375,0.4625,0.4375,0.4625,0.4875,0.4875,0.3625,0.4125,0.4875,0.4625,1.0,0.4625,0.4625,0.3875,0.2375,0.4875,0.4375,0.4625,0.4625,0.4125,0.4625,1.0,0.3375,0.3375,0.4625
-164934002,0.3,0.5,0.5,0.3,0.4,0.3,0.35,0.35,0.475,0.425,0.35,0.375,0.3375,0.375,0.3,0.45,0.4,0.35,0.4,0.3,0.3,0.25,0.375,0.3375,1.0,0.5,0.375
-59931005,0.3,0.5,0.5,0.3,0.4,0.3,0.35,0.35,0.475,0.425,0.35,0.375,0.3375,0.375,0.3,0.45,0.4,0.35,0.4,0.3,0.3,0.25,0.375,0.3375,0.5,1.0,0.375
-17338001,0.425,0.375,0.375,0.425,0.475,0.425,0.475,0.475,0.4,0.45,0.475,0.5,0.4625,1.0,0.425,0.425,0.275,0.475,0.475,0.425,0.425,0.375,0.5,0.4625,0.375,0.375,1.0"""))
 
-# alias
-scoring_matrix_cinc2020 = weights_cinc2020
+dms = dx_mapping_scored.copy()
+dms['scored'] = True
+dmn = dx_mapping_unscored.copy()
+dmn['Notes'] = ''
+dmn['scored'] = False
+dx_mapping_all = pd.concat([dms, dmn], ignore_index=True).fillna('')
+
+
+df_weights_snomed = df_weights  # alias
+
+
+snomed_ct_code_to_abbr = \
+    lambda i: dx_mapping_all[dx_mapping_all["SNOMED CT Code"]==int(i)]["Abbreviation"].values[0]
+
+df_weights_abbr = df_weights.copy()
+
+df_weights_abbr.columns = \
+    df_weights_abbr.columns.map(lambda i: snomed_ct_code_to_abbr(i))
+
+df_weights_abbr.index = \
+    df_weights_abbr.index.map(lambda i: snomed_ct_code_to_abbr(i))
+
+
+
+def load_weights(classes:Sequence[Union[int,str]]=None, return_fmt:str='np') -> Union[np.ndarray, pd.DataFrame]:
+    """ finished, checked,
+
+    load the weight matrix of the `classes`
+
+    Parameters:
+    -----------
+    classes: sequence of str or int, optional,
+        the classes to load their weights,
+        if not given, weights of all classes in `dx_mapping_scored` will be loaded
+    return_fmt: str, default 'np',
+        'np' or 'pd', the values in the form of a 2d array or a DataFrame
+
+    Returns:
+    --------
+    mat: 2d array or DataFrame,
+        the weight matrix of the `classes`
+    """
+    if classes:
+        l_nc = [_normalize_class(c) for c in classes]
+        assert len(set(l_nc)) == len(classes), "`classes` has duplicates!"
+        mat = df_weights_abbr.loc[l_nc,l_nc]
+    else:
+        mat = df_weights_abbr.copy()
+    
+    if return_fmt.lower() == 'np':
+        mat = mat.values
+    elif return_fmt.lower() == 'pd':
+        mat.columns = list(map(str, classes))
+        mat.index = list(map(str, classes))
+    else:
+        raise ValueError(f"format of {return_fmt} is unsupported")
+    
+    return mat
+
+
+def _normalize_class(c:Union[str,int]) -> str:
+    """ finished, checked,
+
+    normalize the class name to its abbr.,
+    facilitating the computation of the `load_weights` function
+
+    Parameters:
+    -----------
+    c: str or int,
+        abbr. or SNOMED CT Code of the class
+
+    Returns:
+    --------
+    nc: str,
+        the abbr. of the class
+    """
+    try:
+        nc = snomed_ct_code_to_abbr(c)
+    except:
+        nc = c
+    if nc not in df_weights_abbr.columns:
+        raise ValueError(f"class {c} not among the scored classes")
+    return nc
