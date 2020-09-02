6f0533f9c530042ca0526fa5628fbb3d4984b584
WEN Hao <wenh06@gmail.com>
2020-09-02 16:44:04 +0800

    add plot_confusion_matrix, not checked

--- utils_misc/utils_viz.py
+++ utils_misc/utils_viz.py
@@ -3,7 +3,7 @@
 utilities for visualization
 """
 from numbers import Real
-from typing import Union, Optional, List, Tuple, NoReturn
+from typing import Union, Optional, List, Tuple, Sequence, NoReturn, Any
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -16,6 +16,7 @@ from ..common import ArrayLike
 __all__ = [
     "plot_single_lead_ecg",
     "plot_hypnogram",
+    "plot_confusion_matrix",
 ]
 
 
@@ -123,3 +124,78 @@ def plot_hypnogram(sleep_stage_curve:ArrayLike, style:str='original', **kwargs) 
         pass
 
     raise NotImplementedError
+
+
+def plot_confusion_matrix(y_true:ArrayLike, y_pred:ArrayLike, classes:Sequence[str], normalize:Optional[str]=None, title:Optional[str]=None, save_path:Optional[str]=None, cmap:Optional[Any]=None) -> Any:
+    """ finished, not checked,
+
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    In case `sklearn` has too low version to have this function
+
+    Parameters:
+    -----------
+    to write
+    """
+    from sklearn.metrics import confusion_matrix
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred, labels=classes, normalize=normalize)
+
+    # try:
+    #     from sklearn.metrics import ConfusionMatrixDisplay
+    #     disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=classes)
+    #     disp = disp.plot(
+    #         include_values=True,
+    #         cmap=(cmap or plt.cm.Blues),
+    #         xticks_rotation=30,
+    #         values_format=('.2f' if normalize else 'd'),
+    #     )
+    #     return disp
+    # except:
+    #     pass
+
+    if not title:
+        if normalize:
+            title = f'Normalized confusion matrix (along {normalize})'
+        else:
+            title = 'Confusion matrix'
+
+    fig, ax = plt.subplots(figsize=(5, 5), dpi=150)
+    im = ax.imshow(cm, interpolation='nearest', cmap=(cmap or plt.cm.Blues))
+    # ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(
+        xticks=np.arange(cm.shape[1]),
+        yticks=np.arange(cm.shape[0]),
+        # ... and label them with the respective list entries
+        xticklabels=classes, yticklabels=classes,
+        # title=title,
+        # ylabel='True label',
+        # xlabel='Predicted label',
+    )
+    ax.set_title(title, fontsize=18)
+    ax.set_xlabel("True severity",fontsize=14)
+    ax.set_ylabel("Predicted severity",fontsize=14)
+    ax.tick_params(axis = 'both', which = 'major', labelsize = 16)
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=30, ha="right", rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(
+                j, i, format(cm[i, j], fmt),
+                ha="center", va="center",
+                color="white" if cm[i, j] > thresh else "black", 
+                fontsize=18
+            )
+    fig.tight_layout()
+
+    if save_path:
+        plt.savefig(save_path, bbox_inches='tight', transparent=True)
+
+    return ax
