53514250eee175ad7a6d9baf494ce5b84f6c9cd9
WEN Hao <wenh06@gmail.com>
2020-10-09 21:37:42 +0800

    improve local fuzzy match

--- utils_universal/utils_str.py
+++ utils_universal/utils_str.py
@@ -248,23 +248,39 @@ def local_fuzzy_match_1(query_string:str, large_string:str, threshold:float=0.8,
     ----------
     https://stackoverflow.com/questions/17740833/checking-fuzzy-approximate-substring-existing-in-a-longer-string-in-python
     """
+    if verbose >= 2:
+        print("-"*100)
+        print("-"*100)
+        print("in local_fuzzy_match_1 (using `fuzz`)")
+        print(f"input query_string = \042{query_string}\042")
+        print(f"result of `process.extractBests` is {process.extractBests(query_string, (large_string,), score_cutoff=threshold)}")
     result = []
     local_scores = []
     for word, _ in process.extractBests(query_string, (large_string,), score_cutoff=threshold):
-        max_l_dist = max(1, int(len(query_string)*(1-threshold)))
+        max_l_dist = max(2, int(len(query_string)*(1-threshold)))
+        search_params = \
+            LevenshteinSearchParams(None,None,None,max_l_dist)
+        if verbose >= 2:
+            print("-"*100)
+            print(f"search_params for `find_near_matches` are\n(max_substitutions = {search_params.unpacked[0]}, max_insertions = {search_params.unpacked[1]}, max_deletions = {search_params.unpacked[2]}, max_l_dist = {search_params.unpacked[3]}),")
+            print(f"hence the method for `find_near_matches` is {_choose_search_class(search_params)}")
         for match in find_near_matches(query_string, word, max_l_dist=max_l_dist):
             match = word[match.start: match.end]
             start = large_string.find(match)
-            score = fuzz.token_set_ratio(match, query_string, force_ascii=False)
             result.append([match, start, start+len(match)])
+            score = _chn_set_ratio(match, query_string)
             if verbose >= 2:
-                print(f"match = {match} for word = {word}, with score = {score}")
+                print(f"match = \042{match}\042 for word = \042{word}\042, with score = {score}")
             local_scores.append(score)
     if best_only and len(result) > 0:
         best_idx = np.argmax(local_scores)
         result = result[best_idx]
     elif best_only and len(result) == 0:
         result = ["", -1, -1]
+    if verbose >= 2:
+        print(f"final result = {result}")
+        print("-"*100)
+        print("-"*100)
     return result
 
 
@@ -299,7 +315,23 @@ def local_fuzzy_match_2(query_string:str, large_string:str, threshold:float=0.8,
     ----------
     https://stackoverflow.com/questions/17740833/checking-fuzzy-approximate-substring-existing-in-a-longer-string-in-python
     """
-    words = large_string.split()
+    _puncs_ = ["；", "。"]
+    # words = large_string.split()
+    words = []
+    start = 0
+    for s in re.finditer("|".join(_puncs_), large_string):
+        end = s.start()
+        words.append(large_string[start:end])
+        start = s.end()
+    if start < len(large_string):
+        end = len(large_string)
+        words.append(large_string[start:end])
+    if verbose >= 2:
+        print("-"*100)
+        print("-"*100)
+        print("in _local_fuzzy_match_2 (using `difflib`)")
+        print(f"input query_string = \042{query_string}\042")
+        print(f"`words` = {words}")
     result = []
     local_scores = []
     for word in words:
@@ -313,21 +345,28 @@ def local_fuzzy_match_2(query_string:str, large_string:str, threshold:float=0.8,
             word_res.append(word[i:i+n])
             starts.append(i+large_string.find(word))
             ends.append(i+n+large_string.find(word))
-        match = "".join(word_res)
-        ratio = len(match) / float(len(query_string))
+        match = ''.join(word_res)
+        ratio = len(extract_chinese(match)) / len(extract_chinese(query_string))
         if verbose >= 2:
-            print(f"match = {match} for word = {word}, with ratio = {ratio}")
+            print("-"*100)
+            print(f"match = \042{match}\042 for word = \042{word}\042, with ratio = {ratio}")
+            print(f"the detailed matching blocks are {word_res}")
         if ratio >= threshold:
             start = min(starts)
             end = max(ends)
             match = large_string[start: end]
             result.append([match, start, end])
-            local_scores.append(fuzz.token_set_ratio(match, query_string, force_ascii=False))
+            score = _chn_set_ratio(match, query_string)
+            local_scores.append(score)
     if best_only and len(result) > 0:
         best_idx = np.argmax(local_scores)
         result = result[best_idx]
     elif best_only and len(result) == 0:
         result = ["", -1, -1]
+    if verbose >= 2:
+        print(f"final result = {result}")
+        print("-"*100)
+        print("-"*100)
     return result
 
 
@@ -361,11 +400,19 @@ def local_fuzzy_match(query_string:str, large_string:str, threshold:float=0.8, v
     ----------
     https://stackoverflow.com/questions/17740833/checking-fuzzy-approximate-substring-existing-in-a-longer-string-in-python
     """
+    if threshold < 0.7:
+        match, start, end = local_fuzzy_match_2(
+            query_string, large_string, threshold,
+            best_only=True, verbose=verbose,
+        )
+        return [match, start, end]
     match_1, start_1, end_1 = local_fuzzy_match_1(
-        query_string, large_string, threshold, best_only=True, verbose=verbose,
+        query_string, large_string, threshold,
+        best_only=True, verbose=verbose,
     )
     match_2, start_2, end_2 = local_fuzzy_match_2(
-        query_string, large_string, threshold, best_only=True, verbose=verbose,
+        query_string, large_string, threshold,
+        best_only=True, verbose=verbose,
     )
     if len(match_1) == 0:
         match, start, end = match_2, start_2, end_2
@@ -375,11 +422,13 @@ def local_fuzzy_match(query_string:str, large_string:str, threshold:float=0.8, v
         start = max(start_1, start_2)
         end = min(end_1, end_2)
         if start < end:
-            match = large_string[start: end]
+            match = large_string[start:end]
         else:
-            score_1 = fuzz.token_set_ratio(match_1, large_string, force_ascii=False)
-            score_2 = fuzz.token_set_ratio(match_2, large_string, force_ascii=False)
-            if score_1 > score_2:
+            score1 = \
+                fuzz.token_set_ratio(match_1, large_string, force_ascii=False)
+            score2 = \
+                fuzz.token_set_ratio(match_2, large_string, force_ascii=False)
+            if score1 > score2:
                 match, start, end = match_1, start_1, end_1
             else:
                 match, start, end = match_2, start_2, end_2
@@ -405,3 +454,44 @@ def extract_chinese(texts:str) -> str:
     """
     pure_chinese_texts = "".join(re.findall("[\u4e00-\u9FFF0-9]", texts))
     return pure_chinese_texts
+
+
+def _chn_set_ratio(s1:str, s2:str) -> int:
+    """
+    """
+    # consider fuzz.partial_token_set_ratio ?
+    csr = fuzz.token_set_ratio(
+        extract_chinese(s1),
+        extract_chinese(s2),
+        force_ascii=False,
+    )
+    return csr
+
+
+def _choose_search_class(search_params:LevenshteinSearchParams) -> str:
+    """
+    modified from `fuzzysearch.choose_search_class`
+    """
+    max_substitutions, max_insertions, max_deletions, max_l_dist = search_params.unpacked
+
+    # if the limitations are so strict that only exact matches are allowed,
+    # use search_exact()
+    if max_l_dist == 0:
+        return "ExactSearch"
+
+    # if only substitutions are allowed, use find_near_matches_substitutions()
+    elif max_insertions == 0 and max_deletions == 0:
+        return "SubstitutionsOnlySearch"
+
+    # if it is enough to just take into account the maximum Levenshtein
+    # distance, use find_near_matches_levenshtein()
+    elif max_l_dist <= min(
+        (max_substitutions if max_substitutions is not None else (1 << 29)),
+        (max_insertions if max_insertions is not None else (1 << 29)),
+        (max_deletions if max_deletions is not None else (1 << 29)),
+    ):
+        return "LevenshteinSearch"
+
+    # if none of the special cases above are met, use the most generic version
+    else:
+        return "GenericSearch"
